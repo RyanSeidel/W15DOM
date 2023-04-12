13b10ccfdd9b08dc100d872cfe5711f8353f8df9
RyanGrieb <ryanrhin@gmail.com>
2023-04-12 16:18:31 -0500

    Add navbar to login page
    remove test code

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify, session
+from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify, session, make_response
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from website.model import db, userinfo, Game, UserGame, Platform
