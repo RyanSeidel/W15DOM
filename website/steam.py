c8135c46bd41c3f766f6b7290987cfa3cae6c4ca
NuttyRyan <76074138+woahitsryan@users.noreply.github.com>
2023-04-23 02:33:48 -0500

    saving

--- website/steam.py
+++ website/steam.py
@@ -1,6 +1,10 @@
 from website.model import Game, UserGame, Platform, userinfo, db
 import steam.webapi
 from flask import flash
+from datetime import datetime
+
+# rest of the code
+
 
 api_key = "4A26AAF6AC8E26A257000E3F51E3AF2B"
 steam_api = steam.webapi.WebAPI(api_key)
@@ -32,21 +36,26 @@ def get_owned_games(platform_key):
                     return False
             return True
 
-
         for game in games:
             print(f"Processing game: {game['name']}")
             game_id = game['appid']
             name = game['name']
 
             if not is_game(game['name']):
-                print(f"Skipping game: {game['name']}")
+                print(f"Skipping game (excluded keyword found): {game['name']}")
                 continue
 
             playtime = game.get('playtime_forever', 0)
 
+            # get last played time for game
+            rtime_last_played = game.get('rtime_last_played', None)
+            last_played = None
+            if rtime_last_played:
+                last_played = datetime.fromtimestamp(rtime_last_played)
+                print(f"Last played for {name}: {last_played}")
+
             # check if game with same external_id already exists
             existing_game = Game.query.filter_by(external_id=str(game_id)).first()
-
 
             if existing_game:
                 # update attributes of existing game
@@ -60,25 +69,16 @@ def get_owned_games(platform_key):
                 if existing_user_game:
                     # update existing user_game
                     existing_user_game.playtime = playtime
+                    existing_user_game.last_played = last_played
                 else:
                     # create new user_game
                     print(f"Adding new game: {name}")
-                    new_user_game = UserGame(platform_id=platform.id, game_id=existing_game.id, playtime=playtime, owned=True, user_id=platform.user_id)
+                    new_user_game = UserGame(platform_id=platform.id, game_id=existing_game.id, playtime=playtime, owned=True, user_id=platform.user_id, last_played=last_played)
                     db.session.add(new_user_game)
-            else:
-                # create new game
-                print(f"Adding new game: {name}")
-                new_game = Game(user_id=platform.user_id, name=name, platform_id=platform.id, genre=game.get('genre', ''), console='PC', completed=False, external_id=game_id)
-                new_game.image_url = f"https://steamcdn-a.akamaihd.net/steam/apps/{new_game.external_id}/header.jpg"
-                db.session.add(new_game)
-                db.session.flush()
-
-                # create new user_game
-                new_user_game = UserGame(platform_id=platform.id, game_id=new_game.id, playtime=playtime, owned=True, user_id=platform.user_id)
-                db.session.add(new_user_game)
 
         # persist changes to the database
         db.session.commit()
+
 
         flash(f"Successfully retrieved {len(games)} owned games for platform {platform.key}", 'success')
         print(f"Retrieved games: {len(games)}")
